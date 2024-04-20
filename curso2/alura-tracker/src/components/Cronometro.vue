757ec2b67a8d3214b31da03f396d8d733618195f
Altamiro Rodrigues <altamiro27@gmail.com>
2024-04-20 20:55:10 -0300

    Novos arquivos
    separando os componentes do formulario

---
+++ curso2/alura-tracker/src/components/Cronometro.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <strong>{{ tempoDecorrido }}</strong>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'CronometroF',
+  props: {
+    tempoEmSegundos: {
+      type: Number,
+      default: 0,
+      required: true,
+    }
+  },
+  computed: {
+    tempoDecorrido(): string {
+      return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11, 8)
+    }
+  }
+})
+</script>
--- curso2/alura-tracker/src/components/Formulario.vue
+++ curso2/alura-tracker/src/components/Formulario.vue
@@ -5,23 +5,7 @@
         <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?" />
       </div>
       <div class="column">
-        <div class="is-flex is-align-items-center is-justify-content-space-between">
-          <section>
-            <strong>{{ tempoDecorrido }}</strong>
-          </section>
-          <button class="button" @click="iniciar">
-            <span class="icon">
-              <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-          </button>
-          <button class="button" @click="finalizar">
-            <span class="icon">
-              <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-          </button>
-        </div>
+        <Temporizador />
       </div>
     </div>
   </div>
@@ -29,29 +13,12 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Temporizador from './Temporizador.vue';
 
 export default defineComponent({
   name: "FormularioTarefa",
-  data() {
-    return {
-      tempoEmSegundos: 0,
-      cronometro: 0
-    }
-  },
-  computed: {
-    tempoDecorrido(): string {
-      return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11, 8)
-    }
-  },
-  methods: {
-    iniciar() {
-      this.cronometro = setInterval(() => {
-        this.tempoEmSegundos += 1
-      }, 1000)
-    },
-    finalizar() {
-      clearInterval(this.cronometro)
-    }
+  components: {
+    Temporizador
   }
 })
 
